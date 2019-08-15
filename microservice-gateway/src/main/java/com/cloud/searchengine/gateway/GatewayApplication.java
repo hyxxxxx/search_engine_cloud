0cdd545e088e1ccfc8847596d18e53adde2b8f14
yasin <357519203@qq.com>
2019-08-15 16:57:13 +0800

    update

--- microservice-gateway/src/main/java/com/cloud/searchengine/gateway/GatewayApplication.java
+++ microservice-gateway/src/main/java/com/cloud/searchengine/gateway/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.cloud.searchengine.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
 }
--- microservice-gateway/src/main/java/com/cloud/searchengine/gateway/route/RouteConfig.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.searchengine.gateway.route;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RouteConfig {
-
-}
